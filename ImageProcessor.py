0aa3fbd44d4a6705029d14e59aef9ab21bd73f48
Kris <87560373+KrisVincent@users.noreply.github.com>
2023-10-24 16:48:46 +0800

    Completed ImageProcessor.py
    May need additional bug checking and possible code review.
    Was able to capture the 5 tags

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -2,24 +2,52 @@ import cv2
 import numpy as np
 
 class ImageProcessor:
-    def __init__(self) -> None:
-        self.__screenshot = None
 
+    # Constants
+    WIDTH_MIN = 200
+    WIDTH_MAX = 225
+    HEIGHT_MIN = 60
+    HEIGHT_MAX = 80
+    Y_MIN = 520
+
+    def __init__(self) -> None:
+
+        # Initialize Properties
+        self.__screenshot = None
+        self.__contours = [] 
+
+    # Set screenshot
     def set_screenshot(self, screenshot):
         self.__screenshot = screenshot
 
+    # Set contours
+    def set_contours(self, contours):
+        self.__contours = contours
+
+    # Get screenshot
     @property
     def screenshot(self):
         return self.__screenshot
+    
+    # Get Contours
+    @property
+    def contours(self):
+        return self.__contours
+
 
     def process_screenshot(self):
+        """
+        Process the screenshot to find contours based on specific conditions.
+        """
 
-        # Check if screenshot has been set
-        if self.screenshot is None:
-            print("Set Screenshot first")
+        # Set variables
+        captured_contours = []
 
         # Get image
         img = cv2.imread(self.screenshot)
+
+        if self.screenshot is None:
+            print("Set Screenshot first")
 
         # Convert colored to gray scaled image
         gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -27,16 +55,40 @@ class ImageProcessor:
         # Apply threshing
         ret, thresh = cv2.threshold(gray_image, 95, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU) 
 
-        # Display thresh
-        self._display_image(thresh)
-
         # Apply Opening
         kernel = np.ones((5,5),np.uint8)
         opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 
-        # Display thresh
-        self._display_image(opening)
+        # Get Contours
+        contours = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours = contours[0] if len(contours) == 2 else contours[1]
 
+        # Loop through the contours
+        for c in contours:
+            x,y,w,h = cv2.boundingRect(c)
+
+            # Check your conditions
+            if self.WIDTH_MIN < w < self.WIDTH_MAX and self.HEIGHT_MIN < h < self.HEIGHT_MAX and y > self.Y_MIN:
+                # Append contours
+                captured_contours.append(c)
+
+        # Pass to contour property
+        self.set_contours(captured_contours)
+
+        # Draw contours
+        self.draw_contours(img)
+
+    # Draw Contours
+    def draw_contours(self, image):
+
+        for contour in self.contours:
+
+            x, y, w, h = cv2.boundingRect(contour)
+
+            # Draw Contours
+            result = cv2.rectangle(image, (x, y), (x + w, y + h), (255,0,0), 5)
+
+        self._display_image(result)
 
     # Debugging purposes
     def _display_image(self, image, windows_title = "Debug", scale_percent = 50):
@@ -49,3 +101,4 @@ class ImageProcessor:
         resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
         cv2.imshow(windows_title,resized)
         cv2.waitKey(0)
+        cv2.destroyAllWindows()
